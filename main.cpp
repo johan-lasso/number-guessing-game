51a4889f9d4ecc419e65f5f9cf73bb38e10d1d83
johan-lasso <lassojohanrendon@gmail.com>
2024-07-26 11:41:53 -0500

    feat: add number evaluation

--- main.cpp
+++ main.cpp
@@ -3,9 +3,39 @@
 
 int genNumber() {
     srand(time(0));
+
     return rand() % 10; // Returns a random number between 0 and 10
 }
 
+void checkIfEqual(int attempts, int random) {
+    int userGuess;
+
+    while(attempts > 0) {
+        std::cin >> userGuess;
+
+        if(userGuess == random) {
+            std::cout << "Congrats!\nYou've guessed it!\n";
+            break;
+        } else {
+            attempts--;
+            if(attempts >= 1 && attempts < 3) 
+                std::cout << "Incorrect!\nCome on, try one more time: ";
+            else 
+                std::cout << "Oh! You did not guessed it :(\n";
+        }
+    }
+}
+
+void init() {
+    int userAttempts = 3;
+    int random = genNumber();
+
+    std::cout << "Hello!\nPlease try to guess what number I'm thinking of. \n"; 
+    std::cout << "Okay, try: "; 
+
+    checkIfEqual(userAttempts, random);
+}
+
 int main() {    
-    std::cout << genNumber();
+    init();
 }
